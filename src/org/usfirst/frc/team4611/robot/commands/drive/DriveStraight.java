0b7cfb0b4e2a357aaab5a9aa9ed5d977415a95cd
sethstobart <seth.stobart@gmail.com>
2018-01-26 15:23:08 -0500

    Command code for DriveStrafe and DriveStraight

---
+++ src/org/usfirst/frc/team4611/robot/commands/drive/DriveStraight.java
@@ -0,0 +1,36 @@
+package org.usfirst.frc.team4611.robot.commands.drive;
+
+import org.usfirst.frc.team4611.robot.Robot;
+import org.usfirst.frc.team4611.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class DriveStraight extends Command{
+	private int positionUnits;
+	private int cpr;
+
+	public DriveStraight(double rotations) {
+		this.requires(Robot.tankDrive);
+		cpr = 1440;
+		positionUnits = (int) Math.round(rotations * cpr);
+	}
+	
+	protected void initialize() {
+		RobotMap.driveTrainBL_Talon.setSelectedSensorPosition(0, 0, 0);
+		RobotMap.driveTrainBR_Talon.setSelectedSensorPosition(0, 0, 0);
+		RobotMap.driveTrainFR_Talon.setSelectedSensorPosition(0, 0, 0);
+		RobotMap.driveTrainFL_Talon.setSelectedSensorPosition(0, 0, 0);		
+	}
+	
+	protected void execute () {
+		System.out.println("Target Pos: " + positionUnits + " Current Position(BL): " + RobotMap.driveTrainBL_Talon.getSelectedSensorPosition(0));
+		Robot.tankDrive.motionMagicStraight(positionUnits);
+	}
+	
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
