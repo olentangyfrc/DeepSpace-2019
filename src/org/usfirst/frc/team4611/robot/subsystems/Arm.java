b11fb2474a8b51a5188844d11779f8bae15e4238
Connor2573 <connor.park2573@gmail.com>
2018-02-04 12:38:35 -0500

    added code to fix load issues between the LAs

--- src/org/usfirst/frc/team4611/robot/subsystems/Arm.java
+++ src/org/usfirst/frc/team4611/robot/subsystems/Arm.java
@@ -21,7 +21,7 @@ public class Arm extends Subsystem {
 		double potValue2 = RobotMap.linearActuatorPot2.get();
 		double pos1 = (potValue1 - min1) / (max1 - min1);
 		double pos2 = (potValue2 - min2) / (max2 - min2);
-		if((potValue1 < max1 && pos1 <= pos2 + VARIANCE_LIMIT)) {
+		if(potValue1 < max1 && pos1 <= pos2 + VARIANCE_LIMIT) {
 			//RobotMap.linearActuator.set(RobotMap.linearActuatorControl, speed);
 			RobotMap.linearActuator.set(speed);
 		}
@@ -30,13 +30,46 @@ public class Arm extends Subsystem {
 			RobotMap.linearActuator.set(0);
 			
 		}
-		if((potValue2 < max2 && pos2 <= pos1 + VARIANCE_LIMIT)) {
+		if(potValue2 < max2 && pos2 <= pos1 + VARIANCE_LIMIT) {
 			RobotMap.linearActuator2.set(speed2);
 		}
 		else {
 			RobotMap.linearActuator2.set(0);
 		}
 	}
+	
+	
+	
+	public void movePotDown(double speed, double speed2) {
+		//System.out.println("\n"+"LinearActuatorPot: "+ RobotMap.linearActuatorPot.get());
+		//System.out.println("Shuffleboard value: "+ RobotMap.getValue(RobotMap.potentiometerSubTable, RobotMap.potMinID));
+		double max1 = (double)RobotMap.getValue(RobotMap.potentiometerSubTable, RobotMap.potMaxID);
+		double min1 = (double)RobotMap.getValue(RobotMap.potentiometerSubTable, RobotMap.potMinID);
+		double max2 = (double)RobotMap.getValue(RobotMap.potentiometerSubTable, RobotMap.potMax2ID);
+		double min2 = (double)RobotMap.getValue(RobotMap.potentiometerSubTable, RobotMap.potMin2ID);
+		double potValue1 = RobotMap.linearActuatorPot.get();
+		double potValue2 = RobotMap.linearActuatorPot2.get();
+		double pos1 = (potValue1 - min1) / (max1 - min1);
+		double pos2 = (potValue2 - min2) / (max2 - min2);
+		
+		if((potValue1 > min1 && pos1 >= pos2 - VARIANCE_LIMIT)) {
+			//talon and victor code
+			//RobotMap.linearActuator.set(RobotMap.linearActuatorControl, speed);
+			RobotMap.linearActuator.set(-speed);
+		}
+		else {
+			//talon and victor code
+			//RobotMap.linearActuator.set(RobotMap.linearActuatorControl, 0);
+			RobotMap.linearActuator.set(0);
+		}
+		if(potValue2 > min2 && pos2 >= pos1 - VARIANCE_LIMIT) {
+				RobotMap.linearActuator2.set(-speed2);
+		}
+		else {
+			RobotMap.linearActuator2.set(0);
+		}
+	}
+	
 	
 	public void movePotSwitch(double speed, double speed2) {
 		if((RobotMap.linearActuatorPot.get() < (double)RobotMap.getValue(RobotMap.potentiometerSubTable, RobotMap.potSwitch2ID))) {
@@ -57,28 +90,6 @@ public class Arm extends Subsystem {
 		}
 	}
 	
-	public void movePotDown(double speed, double speed2) {
-		System.out.println("\n"+"LinearActuatorPot: "+ RobotMap.linearActuatorPot.get());
-		System.out.println("Shuffleboard value: "+ RobotMap.getValue(RobotMap.potentiometerSubTable, RobotMap.potMinID));
-		
-		if((RobotMap.linearActuatorPot.get() > (double)RobotMap.getValue(RobotMap.potentiometerSubTable, RobotMap.potMinID))) {
-			//talon and victor code
-			//RobotMap.linearActuator.set(RobotMap.linearActuatorControl, speed);
-			RobotMap.linearActuator.set(-speed);
-		}
-		else {
-			//talon and victor code
-			//RobotMap.linearActuator.set(RobotMap.linearActuatorControl, 0);
-			RobotMap.linearActuator.set(0);
-		}
-		if(RobotMap.linearActuatorPot2.get() > (double)RobotMap.getValue(RobotMap.potentiometerSubTable, RobotMap.potMin2ID)) {
-				RobotMap.linearActuator2.set(-speed2);
-		}
-		else {
-			RobotMap.linearActuator2.set(0);
-		}
-	}
-
 	@Override
 	protected void initDefaultCommand() {	
 	}
