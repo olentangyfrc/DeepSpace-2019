29bec6c7f15b30b58cfd89d5196428fea6cfd36c
sethstobart <seth.stobart@gmail.com>
2018-02-13 17:49:15 -0500

    Added magic shapes package and the MovePotPos java

---
+++ src/org/usfirst/frc/team4611/robot/commands/magicshapes/StartingPos.java
@@ -0,0 +1,13 @@
+package org.usfirst.frc.team4611.robot.commands.magicshapes;
+
+import org.usfirst.frc.team4611.robot.commands.elevator.MoveElevatorToPos;
+//import org.usfirst.frc.team4611.robot.potentiometer.MovePotPos;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class StartingPos extends CommandGroup {
+	public StartingPos() {
+		addParallel(new MoveElevatorToPos(0));
+		//addParallel(new MovePotPos(.8));
+	}
+}
---
+++ src/org/usfirst/frc/team4611/robot/potentiometer/MovePotPos.java
@@ -0,0 +1,24 @@
+/*package org.usfirst.frc.team4611.robot.potentiometer;
+
+import org.usfirst.frc.team4611.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class MovePotPos extends Command{
+
+	private static double position;
+	
+	public MovePotPos(double position) {
+		this.position = position;
+	}
+	protected void execute() {
+		Robot.arm.movePotPos(position);
+	}
+	
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}*/
