23eb7d9bbbda2cd4233516ef7bfa3717b7a4908d
Connor2573 <connor.park2573@gmail.com>
2018-02-14 17:11:27 -0500

    LA movements no isFinisheds yet next commit will add that

--- src/org/usfirst/frc/team4611/robot/subsystems/Arm.java
+++ src/org/usfirst/frc/team4611/robot/subsystems/Arm.java
@@ -66,15 +66,44 @@ public class Arm extends Subsystem {
 	}
 	
 	public void movePotPos(double pos) {
+		double max1 = (double)RobotMap.getValue(RobotMap.potentiometerSubTable, RobotMap.potMaxID);
+		double min1 = (double)RobotMap.getValue(RobotMap.potentiometerSubTable, RobotMap.potMinID);
+		double max2 = (double)RobotMap.getValue(RobotMap.potentiometerSubTable, RobotMap.potMax2ID);
+		double min2 = (double)RobotMap.getValue(RobotMap.potentiometerSubTable, RobotMap.potMin2ID);
+		double potValue1 = RobotMap.linearActuatorPot.get();
+		double potValue2 = RobotMap.linearActuatorPot2.get();
+		double pos1 = (potValue1 - min1) / (max1 - min1);
+		double pos2 = (potValue2 - min2) / (max2 - min2);
 		
 		if (RobotMap.linearActuator.get() > pos + 0.05 || RobotMap.linearActuator.get() > pos - 0.05) {
-			RobotMap.linearActuator.set(-0.7);
-			RobotMap.linearActuator2.set(-0.7);		
+			if((potValue1 > min1 && pos1 >= pos2 - varianceLimit)) {
+				RobotMap.linearActuator.set(-.7);
+			}
+			else {
+				RobotMap.linearActuator.set(0);
+			}
+			if(potValue2 > min2 && pos2 >= pos1 - varianceLimit) {
+					RobotMap.linearActuator2.set(-.7);
+			}
+			else {
+				RobotMap.linearActuator2.set(0);
+			}
 		}
 		
 		else if (RobotMap.linearActuator.get() < pos + 0.05 || RobotMap.linearActuator.get() < pos - 0.05) {
-			RobotMap.linearActuator.set(0.7);
-			RobotMap.linearActuator2.set(0.7);
+			if(potValue1 < max1 && pos1 <= pos2 + varianceLimit) {
+				RobotMap.linearActuator.set(.7);
+			}
+			else {
+				RobotMap.linearActuator.set(0);
+				
+			}
+			if(potValue2 < max2 && pos2 <= pos1 + varianceLimit) {
+				RobotMap.linearActuator2.set(.7);
+			}
+			else {
+				RobotMap.linearActuator2.set(0);
+			}
 		}
 		
 		else {
