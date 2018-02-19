6378820d12eddefef735cf573b25cdddb588ee1d
sethstobart <seth.stobart@gmail.com>
2018-02-19 10:37:23 -0500

    Added clmiber backwards file

---
+++ src/org/usfirst/frc/team4611/robot/commands/climber/MoveClimberBackward.java
@@ -0,0 +1,28 @@
+package org.usfirst.frc.team4611.robot.commands.climber;
+
+import org.usfirst.frc.team4611.robot.Robot;
+import org.usfirst.frc.team4611.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class MoveClimberBackward extends Command{
+	
+	public MoveClimberBackward() {
+		this.requires(Robot.climber);
+	}
+	protected void execute() {
+		double speed = (double)RobotMap.getValue(RobotMap.climberSubtable, RobotMap.climberSpeed);		
+		Robot.climber.move(-speed);
+	}
+
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	protected void end() {
+		Robot.climber.move(0);
+	}
+
+}
