59355edde5fa1741d7d67eb5e2dd0ab3425a8d84
Connor2573 <connor.park2573@gmail.com>
2018-02-23 12:42:45 -0500

    added untracked files

---
+++ src/org/usfirst/frc/team4611/robot/commands/auton/StartRightNullZoneRightside.java
@@ -0,0 +1,19 @@
+package org.usfirst.frc.team4611.robot.commands.auton;
+
+import org.usfirst.frc.team4611.robot.commands.elevator.MoveElevatorToPos;
+import org.usfirst.frc.team4611.robot.commands.elevator.ResetElevator;
+import org.usfirst.frc.team4611.robot.commands.solenoid.ExtendSolenoid;
+import org.usfirst.frc.team4611.robot.subsystems.Elevator;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class StartRightNullZoneRightside extends CommandGroup {
+
+	public StartRightNullZoneRightside() {
+		// TODO Auto-generated constructor stub
+		addSequential(new ResetElevator());
+		addSequential(new StopAndRepositionTalons());
+		addSequential(new AutonForward(13*12));
+		addSequential(new StopAndRepositionTalons());
+	}
+}
