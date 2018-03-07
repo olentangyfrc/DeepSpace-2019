ea5f4e4a0aa153e33948745c452e1602831ec92c
sethstobart <seth.stobart@gmail.com>
2018-03-07 12:25:49 -0500

    Threw in some comments b/c should've been there anyways

---
+++ src/org/usfirst/frc/team4611/robot/commands/drive/FindBox.java
@@ -0,0 +1,16 @@
+package org.usfirst.frc.team4611.robot.commands.drive;
+
+import org.usfirst.frc.team4611.robot.commands.auton.StopAndRepositionTalons;
+import org.usfirst.frc.team4611.robot.commands.pigeon.PigeonAdjustVision2;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class FindBox extends CommandGroup {
+
+	public FindBox() {
+		//addSequential(new StopAndRepositionTalons());
+		//addSequential(new VisionHorizontalDrive2());
+		addSequential(new StopAndRepositionTalons());
+		addSequential(new PigeonAdjustVision2());
+	}
+}
