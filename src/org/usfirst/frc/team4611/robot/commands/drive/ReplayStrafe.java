59568c9d8e2670bcbbd7981702d293475d6d2f33
Patrick Worth <worthp@worthconsulting.com>
2018-03-18 16:45:54 -0400

    reworked to do one velocity set per execution

--- src/org/usfirst/frc/team4611/robot/commands/drive/ReplayStrafe.java
+++ src/org/usfirst/frc/team4611/robot/commands/drive/ReplayStrafe.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team4611.robot.commands.drive;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import org.usfirst.frc.team4611.robot.Robot;
 import org.usfirst.frc.team4611.robot.utilities.ArrayReader;
@@ -16,6 +17,9 @@ import edu.wpi.first.wpilibj.command.Command;
 
 public class ReplayStrafe extends Command {
 	int replayInches	= 10 * 12; // 10 feet * 12 inches
+	int iterations	= 0;
+	Iterator<double[]>	velocityIterator	= null;
+	int	velocityCount	= 0;
 	
 	/**
 	 * these arrays needs to be filled in by captured velocities from Mecanum Commannd of 10 feet of accurate Strafing.
@@ -35,10 +39,13 @@ public class ReplayStrafe extends Command {
 	public void initialize() {
 		
 		try {
-			if (tenFeetStrafeLeftVelocities == null)
+			if (tenFeetStrafeLeftVelocities == null) {
 				tenFeetStrafeLeftVelocities		= ArrayReader.getInstance().getNumbers("TenFeetStrafeLeftRecording.txt");
-			if (tenFeetStrafeRightVelocities == null)
+				velocityCount	= tenFeetStrafeLeftVelocities.size();
+			} if (tenFeetStrafeRightVelocities == null) {
 				tenFeetStrafeRightVelocities	= ArrayReader.getInstance().getNumbers("TenFeetStrafeRightRecording.txt");
+				velocityCount	= tenFeetStrafeRightVelocities.size();
+			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
@@ -53,33 +60,33 @@ public class ReplayStrafe extends Command {
 		requires(Robot.mecanum);
 		
 		distanceInInches = inches;
-		dir	= direction;
-	}
+		dir	= direction;// choose the right replay sequence
 
-	public void execute() {
-		
-		// choose the right replay sequence
-		ArrayList<double[]> replay = tenFeetStrafeLeftVelocities;
+		velocityIterator	= tenFeetStrafeLeftVelocities.iterator();
 		if (dir == Direction.RIGHT) {
-			replay	= tenFeetStrafeRightVelocities;
+			velocityIterator	= tenFeetStrafeRightVelocities.iterator();
 		} 
 		
 		// pro-rate replay array by how far this command wants to go
-		int	howMuchToReplay = distanceInInches / replayInches * replay.size();
+		iterations = distanceInInches / replayInches * velocityCount;
+	}
+
+	public void execute() {		
 
 		// holds velocities for 1 iteration
 		double [] velocities;
 		
-		for (int i = 0; i < howMuchToReplay; i++) {
-			velocities	= replay.get(i);
-			Robot.mecanum.velocityDrive(velocities[0], velocities[1], velocities[2], velocities[3]);			
+		if (velocityIterator.hasNext()) {
+			velocities	= velocityIterator.next();
+			Robot.mecanum.velocityDrive(velocities[0], velocities[1], velocities[2], velocities[3]);	
+			iterations -= 1;
 		}
 		
 	}
 
 	@Override
 	protected boolean isFinished() {
-		return true;
+		return (iterations == 0 || !velocityIterator.hasNext());
 	}
 
 	static public enum Direction {RIGHT, LEFT};
