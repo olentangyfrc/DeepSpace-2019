846c53c4bb7a3b44aab92373de35442abc20214a
Connor2573 <connor.park2573@gmail.com>
2018-03-24 12:48:47 -0400

    fixed center commands to move to box on top of box pyramid

--- src/org/usfirst/frc/team4611/robot/commands/auton/dualTargets/CenterRightReset.java
+++ src/org/usfirst/frc/team4611/robot/commands/auton/dualTargets/CenterRightReset.java
@@ -2,6 +2,7 @@ package org.usfirst.frc.team4611.robot.commands.auton.dualTargets;
 
 import org.usfirst.frc.team4611.robot.RobotMap;
 import org.usfirst.frc.team4611.robot.commands.arm.MovePotPos;
+import org.usfirst.frc.team4611.robot.commands.drive.AutonBackward;
 import org.usfirst.frc.team4611.robot.commands.drive.AutonForward;
 import org.usfirst.frc.team4611.robot.commands.drive.StopAndRepositionTalons;
 import org.usfirst.frc.team4611.robot.commands.elevator.ResetElevator;
@@ -29,6 +30,15 @@ public class CenterRightReset extends CommandGroup {
 		addSequential(new MovePotPos(RobotMap.POTSWITCH));
 		addSequential(new AutonForward(RobotMap.HALFWAY+20), 1.5);
 		addSequential(new ReleaseBox());
+		addSequential(new AutonBackward(RobotMap.HALFWAY));
+		addSequential(new StopAndRepositionTalons());
+		addSequential(new PigeonAdjust(RobotMap.turnAngle1));
+		addSequential(new StopAndRepositionTalons());
+		addSequential(new AutonForward(RobotMap.HALFWAY-10));
+		addSequential(new StopAndRepositionTalons());
+		addSequential(new PigeonAdjust(-RobotMap.turnAngle1));
+		addSequential(new StopAndRepositionTalons());
+		addSequential(new AutonForward(RobotMap.HALFWAY), 1);
 	}
 	protected void initialize() {
 		Logger.log("initialized", this.getClass().getName());
