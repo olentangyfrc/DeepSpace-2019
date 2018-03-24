63e1939cc2762fe974209511e11919cda13240d8
Connor2573 <connor.park2573@gmail.com>
2018-03-24 11:06:31 -0400

    added untracked combinations

---
+++ src/org/usfirst/frc/team4611/robot/commands/auton/dualTargets/StartLeftScaleRightScaleRight.java
@@ -0,0 +1,60 @@
+package org.usfirst.frc.team4611.robot.commands.auton.dualTargets;
+
+import org.usfirst.frc.team4611.robot.RobotMap;
+import org.usfirst.frc.team4611.robot.commands.arm.MovePotPos;
+import org.usfirst.frc.team4611.robot.commands.auton.Wait;
+import org.usfirst.frc.team4611.robot.commands.drive.AutonBackward;
+import org.usfirst.frc.team4611.robot.commands.drive.AutonForward;
+import org.usfirst.frc.team4611.robot.commands.drive.AutonStrafeRight;
+import org.usfirst.frc.team4611.robot.commands.drive.FindBox;
+import org.usfirst.frc.team4611.robot.commands.drive.StopAndRepositionTalons;
+import org.usfirst.frc.team4611.robot.commands.elevator.MoveElevatorToPos;
+import org.usfirst.frc.team4611.robot.commands.elevator.ResetElevator;
+import org.usfirst.frc.team4611.robot.commands.pigeon.PigeonAdjust;
+import org.usfirst.frc.team4611.robot.commands.solenoid.GrabBox;
+import org.usfirst.frc.team4611.robot.commands.solenoid.PushBox;
+import org.usfirst.frc.team4611.robot.commands.solenoid.ReleaseBox;
+import org.usfirst.frc.team4611.robot.logging.Logger;
+import org.usfirst.frc.team4611.robot.subsystems.Elevator;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class StartLeftScaleRightScaleRight extends CommandGroup {
+
+	public StartLeftScaleRightScaleRight() {
+		addSequential(new ResetElevator());
+		addSequential(new GrabBox());
+		addSequential(new StopAndRepositionTalons());
+		addSequential(new AutonForward(RobotMap.TOSCALE));
+		addParallel(new MoveElevatorToPos(Elevator.ELEVATOR_TOP));
+		addSequential(new StopAndRepositionTalons());
+		addSequential(new PigeonAdjust(-RobotMap.turnAngle1));
+		addSequential(new StopAndRepositionTalons());
+		addSequential(new AutonForward(RobotMap.HALFWAY/2), 1.5);// 1.85
+		addSequential(new ReleaseBox());
+		addSequential(new PushBox());
+		addSequential(new StopAndRepositionTalons());
+		addSequential(new AutonBackward(RobotMap.HALFWAY/2), 1.5);
+		addSequential(new StopAndRepositionTalons());
+		addSequential(new PigeonAdjust(-RobotMap.turnAngle1));
+		addSequential(new StopAndRepositionTalons());
+		addSequential(new AutonForward(RobotMap.WAY));
+		addSequential(new StopAndRepositionTalons());
+		addSequential(new AutonStrafeRight(30));
+		addSequential(new StopAndRepositionTalons());
+		addSequential(new FindBox());
+		addSequential(new GrabBox());
+		addSequential(new Wait(1));
+		addSequential(new PigeonAdjust(RobotMap.turnAngle2));
+		addSequential(new StopAndRepositionTalons());
+		addParallel(new MoveElevatorToPos(Elevator.ELEVATOR_TOP));
+		addParallel(new MovePotPos(RobotMap.POTMAX));
+		addSequential(new Wait(.1));
+		addSequential(new AutonForward(RobotMap.HALFWAY));
+		addSequential(new StopAndRepositionTalons());
+		addSequential(new ReleaseBox());
+	}
+	protected void initialize() {
+		Logger.log("initialized", this.getClass().getName());
+	}
+}
---
+++ src/org/usfirst/frc/team4611/robot/commands/auton/dualTargets/StartRightScaleLeftScaleLeft.java
@@ -0,0 +1,46 @@
+package org.usfirst.frc.team4611.robot.commands.auton.dualTargets;
+
+import org.usfirst.frc.team4611.robot.RobotMap;
+import org.usfirst.frc.team4611.robot.commands.arm.MovePotPos;
+import org.usfirst.frc.team4611.robot.commands.auton.Wait;
+import org.usfirst.frc.team4611.robot.commands.drive.AutonBackward;
+import org.usfirst.frc.team4611.robot.commands.drive.AutonForward;
+import org.usfirst.frc.team4611.robot.commands.drive.AutonStrafeRight;
+import org.usfirst.frc.team4611.robot.commands.drive.FindBox;
+import org.usfirst.frc.team4611.robot.commands.drive.StopAndRepositionTalons;
+import org.usfirst.frc.team4611.robot.commands.elevator.MoveElevatorToPos;
+import org.usfirst.frc.team4611.robot.commands.elevator.ResetElevator;
+import org.usfirst.frc.team4611.robot.commands.pigeon.PigeonAdjust;
+import org.usfirst.frc.team4611.robot.commands.solenoid.GrabBox;
+import org.usfirst.frc.team4611.robot.commands.solenoid.PushBox;
+import org.usfirst.frc.team4611.robot.commands.solenoid.ReleaseBox;
+import org.usfirst.frc.team4611.robot.logging.Logger;
+import org.usfirst.frc.team4611.robot.subsystems.Elevator;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class StartRightScaleLeftScaleLeft extends CommandGroup {
+
+	public StartRightScaleLeftScaleLeft() {
+		addSequential(new ResetElevator());
+		addSequential(new GrabBox());
+		addSequential(new StopAndRepositionTalons());
+		addParallel(new MoveElevatorToPos(Elevator.ELEVATOR_TOP/2));
+		addParallel(new MovePotPos(RobotMap.POTSWITCH));
+		addSequential(new AutonForward(RobotMap.MOREWAY));
+		addSequential(new StopAndRepositionTalons());
+		addSequential(new PigeonAdjust(-RobotMap.turnAngle1 + 3));
+		addSequential(new StopAndRepositionTalons());
+		addSequential(new AutonForward(RobotMap.crossToScale+40));
+		addSequential(new StopAndRepositionTalons());
+		addSequential(new PigeonAdjust(RobotMap.turnAngle1));
+		addSequential(new StopAndRepositionTalons());
+		addSequential(new AutonForward(55));
+		addSequential(new StopAndRepositionTalons());
+		addSequential(new PigeonAdjust(RobotMap.turnAngle3));
+		addSequential(new ReleaseBox());
+	}
+	protected void initialize() {
+		Logger.log("initialized", this.getClass().getName());
+	}
+}
