89c78391486b674f622dad4accd535997d1de6d9
Connor2573 <connor.park2573@gmail.com>
2018-03-29 10:37:31 -0400

    changed pot max and min

--- src/org/usfirst/frc/team4611/robot/commands/auton/TestBlock.java
+++ src/org/usfirst/frc/team4611/robot/commands/auton/TestBlock.java
@@ -1,16 +1,15 @@
 package org.usfirst.frc.team4611.robot.commands.auton;
 
-import org.usfirst.frc.team4611.robot.commands.drive.StopAndRepositionTalons;
-import org.usfirst.frc.team4611.robot.commands.pigeon.PigeonAdjustVision;
+import org.usfirst.frc.team4611.robot.commands.pigeon.PigeonAdjust;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class TestBlock extends CommandGroup {
 	public TestBlock() {
-		//addSequential(new StopAndRepositionTalons());
-		//addSequential(new VisionHorizontalDrive2());
-		addSequential(new StopAndRepositionTalons());
-		addSequential(new PigeonAdjustVision());
+		addSequential(new PigeonAdjust(90));
+		addSequential(new PigeonAdjust(90));
+		addSequential(new PigeonAdjust(90));
+		addSequential(new PigeonAdjust(90));
 	}
 }
 
