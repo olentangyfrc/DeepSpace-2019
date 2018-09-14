f7015573929918e1a444b30bd274b7b1cff3350e
Connor2573 <connor.park2573@gmail.com>
2018-09-14 11:26:39 -0400

    set up copy from casting branch of 2018 off season

--- src/org/usfirst/frc/team4611/robot/OI.java
+++ src/org/usfirst/frc/team4611/robot/OI.java
@@ -1,24 +1,9 @@
 package org.usfirst.frc.team4611.robot;
 
-import org.usfirst.frc.team4611.robot.commands.arm.MovePotDown;
-import org.usfirst.frc.team4611.robot.commands.arm.MovePotUp;
-import org.usfirst.frc.team4611.robot.commands.climber.MoveClimber;
-import org.usfirst.frc.team4611.robot.commands.climber.WindUpClimber;
-import org.usfirst.frc.team4611.robot.commands.elevator.MoveElevatorDown;
-import org.usfirst.frc.team4611.robot.commands.elevator.MoveElevatorUp;
-import org.usfirst.frc.team4611.robot.commands.elevator.ResetElevator;
-import org.usfirst.frc.team4611.robot.commands.happyshapes.ScalePos;
-import org.usfirst.frc.team4611.robot.commands.happyshapes.StartingPos;
-import org.usfirst.frc.team4611.robot.commands.happyshapes.SwitchPos;
-import org.usfirst.frc.team4611.robot.commands.solenoid.GrabBox;
-import org.usfirst.frc.team4611.robot.commands.solenoid.PushBox;
-import org.usfirst.frc.team4611.robot.commands.solenoid.ReleaseBox;
-import org.usfirst.frc.team4611.robot.commands.solenoid.ToggleGrabber;
-
+import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.buttons.Button;
-import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 
 /**
@@ -28,175 +13,21 @@ import edu.wpi.first.wpilibj.buttons.JoystickButton;
  */
 
 public class OI {
-	
-	//Joysticks
 	public static Joystick leftJoy;
 	public static Joystick rightJoy;
-	public static Joystick auxJoy;
-	public static XboxController con;
 	
-	//Movement Buttons
-	public Button autoGrabBox;
-	public Button aimBox;
-	public Button aimBox2;
-	
-	//Solenoid Buttons
-	public Button grabberToggle;
-	public Button grabberExtend;
-	public Button grabberRetract;
-	public Button grabberExtend2;
-	public Button pushBox;
-	
-	//LA Buttons
-	public Button linearActuatorUp;
-	public Button linearActuatorDown;
-	public Button linearActuatorUp2;
-	public Button linearActuatorDown2;
-	public Button linearActuatorUp3;
-	public Button linearActuatorDown3;
-	public Button linearActuatorSwitch;
-	
-	//Elevator Buttons
-	public Button moveElUp;
-	public Button moveElDown;
-	
-	//Happy Place Buttons
-	public Button happySwitch;
-	public Button happyScale;
-	public Button happyAttack;
-	public Button happyStart;
-    public Button happyReset;
-	
-	//Climber Buttons
-	public Button moveClimber;
-	public Button windClimber;
-	public Button moveClimbertoPos;
-	
-	//Lidar Buttons
-	public Button getDistance;
-	public Button getDistanceController;
-	
-
 	public OI (){
-		
-		//Joystick
-		leftJoy = new Joystick(RobotMap.LEFT_JOY_PORT); //The left joystick exists on this port in robot map
-		rightJoy = new Joystick(RobotMap.RIGHT_JOY_PORT); //The right joystick exists on this port in robot map
-		auxJoy = new Joystick(RobotMap.THIRD_JOY_PORT);
-		con = new XboxController(RobotMap.LEFT_JOY_PORT);
-		
-		//Movement Buttons
-		autoGrabBox = new JoystickButton(auxJoy, 11);
-		aimBox = new JoystickButton(auxJoy, 10);
-		aimBox2 = new JoystickButton(leftJoy, 3);
-		
-		//LA Buttons
-		linearActuatorUp = new JoystickButton(rightJoy, 3);
-		linearActuatorDown = new JoystickButton(rightJoy, 2);
-		linearActuatorUp2 = new JoystickButton(auxJoy, 11);
-		linearActuatorDown2 = new JoystickButton(auxJoy, 10);
-		
-		//Solenoid Buttons
-		grabberToggle = new JoystickButton(leftJoy, 1);
-		grabberExtend = new JoystickButton(leftJoy, 6);//open claw
-		grabberExtend2 = new JoystickButton(auxJoy, 1);
-		grabberRetract = new JoystickButton(leftJoy, 7);//close claw
-		pushBox = new JoystickButton(rightJoy, 1);
-		
-		//Elevator Buttons
-		moveElUp = new JoystickButton (auxJoy, 6);
-		moveElDown = new JoystickButton (auxJoy, 7);
-		
-		//Climber Buttons
-		moveClimber = new JoystickButton(rightJoy, 10);
-		windClimber = new JoystickButton(rightJoy, 11);
-//		moveClimbertoPos = new JoystickButton(rightJoy, 7);
-		
-		
-		//Happy Shaping Buttons
-		happyScale = new JoystickButton(auxJoy,5);                   
-		happySwitch = new JoystickButton(auxJoy,3);
-		happyAttack = new JoystickButton(auxJoy, 4);
-		happyStart = new JoystickButton(auxJoy, 9);
-		happyReset = new JoystickButton(auxJoy, 8);
-		
-		//Lidar Buttons
-		getDistanceController = new JoystickButton(con, 2);
-			
-		//Sends the starting values of the joysticks to the Shuffleboard
-		RobotMap.updateValue(RobotMap.joyStickSubTable, RobotMap.leftJoyXID, leftJoy.getX());
-		RobotMap.updateValue(RobotMap.joyStickSubTable, RobotMap.leftJoyYID, leftJoy.getY());
-		RobotMap.updateValue(RobotMap.joyStickSubTable, RobotMap.leftJoyZID, leftJoy.getZ());
-		RobotMap.updateValue(RobotMap.joyStickSubTable, RobotMap.rightJoyXID, rightJoy.getX());
-		RobotMap.updateValue(RobotMap.joyStickSubTable, RobotMap.rightJoyYID, rightJoy.getY());
-		RobotMap.updateValue(RobotMap.joyStickSubTable, RobotMap.rightJoyZID, rightJoy.getZ());
-
-		//Movement Commands
-//		aimBox.whenPressed(new AimForBox());
-//		aimBox2.whenPressed(new FindBox());//RobotMap.driveTrain));   DRIVER COMMANDS TBD BY DRIVERS
-//		autoGrabBox.whenPressed(new AutoGrab());
-		
-		//LA commands
-		linearActuatorUp.whileHeld(new MovePotUp());
-		linearActuatorUp2.whileHeld(new MovePotUp());			
-		linearActuatorDown.whileHeld(new MovePotDown());
-		linearActuatorDown2.whileHeld(new MovePotDown());
-
-		//Elevator Commands
-		moveElUp.whileHeld(new MoveElevatorUp());	
-		moveElDown.whileHeld(new MoveElevatorDown());
-		
-		//Magic Shaping Commands	
-		happyScale.whenPressed(new ScalePos());
-		happySwitch.whenPressed(new SwitchPos());
-		//happyAttack.whenPressed(new AttackPos());
-		happyStart.whenPressed(new StartingPos());
-		happyReset.whenPressed(new ResetElevator());
-		
-		//Grabber Commands
-		grabberToggle.whenPressed(new ToggleGrabber());
-		grabberExtend.whileHeld(new ReleaseBox());
-		grabberExtend2.whileHeld(new ReleaseBox());
-		grabberRetract.whileHeld(new GrabBox());
-		pushBox.whenPressed(new PushBox());
-		
-		//Climber Commands
-		moveClimber.whileHeld(new MoveClimber());
-		windClimber.whileHeld(new WindUpClimber());
-//		moveClimbertoPos.whenPressed(new ClimberToPos(26050)); Not working feature 
-		
+		leftJoy = new Joystick(0); //The left joystick exists on this port in robot map
+		rightJoy = new Joystick(1); //The right joystick exists on this port in robot map
 	}
 	
-	public double filter(double raw) //We pass joystick values through the filter here and edit the raw value
+	public static double generalJoystickFilter(double raw) //We pass joystick values through the filter here and edit the raw value
     {
-        if (Math.abs(raw) < (double)RobotMap.getValue(RobotMap.mecanumSubTable, RobotMap.deadZoneID)) {
+        if (Math.abs(raw) < .15) {
             return 0; //If the value passed is less than 15% ignore it. This is reffered to as a deadzone
         } else {
-            return  raw * (double)RobotMap.getValue(RobotMap.mecanumSubTable, RobotMap.motorPowerID); //Set the output to a ceratin percent of of the input
+            return  raw * (1); //Set the output to a ceratin percent of of the input
         }
     }
-	
-	public double strafeFilter(double raw) {
-		if (Math.abs(raw) < (double)RobotMap.getValue(RobotMap.mecanumSubTable, RobotMap.deadZoneID)) {
-            return 0;
-        } else {
-            return  raw * Math.min((double)RobotMap.getValue(RobotMap.mecanumSubTable, RobotMap.strafePowerID) * 2, 1); //Set the output to a ceratin percent of of the input
-        }
-	}	
-	
-	public double rotateFilter(double raw) {
-		if (Math.abs(raw) < (double)RobotMap.getValue(RobotMap.mecanumSubTable, RobotMap.deadZoneID)) {
-            return 0;
-        } else {
-            return  raw * 0.5;
-        }
-	}
-	public double LAFilter(double raw) {
-		if (Math.abs(raw) < (double)RobotMap.getValue(RobotMap.mecanumSubTable, RobotMap.deadZoneID)) {
-            return 0;
-        } else {
-            return  raw;// * Math.min((double)RobotMap.getValue(RobotMap.linearActuatorSubTable, RobotMap.LAFilterID), 1); //Set the output to a ceratin percent of of the input
-        }
-	}
 	
 }
--- src/org/usfirst/frc/team4611/robot/Robot.java
+++ src/org/usfirst/frc/team4611/robot/Robot.java
@@ -1,143 +1,56 @@
+
 package org.usfirst.frc.team4611.robot;
 
 import java.util.HashMap;
 
-import org.usfirst.frc.team4611.robot.commands.auton.AutonCommandGroup;
-import org.usfirst.frc.team4611.robot.commands.auton.TestBlock;
-import org.usfirst.frc.team4611.robot.logging.Logger;
-import org.usfirst.frc.team4611.robot.subsystems.Arm;
-import org.usfirst.frc.team4611.robot.subsystems.BoxPusher;
-import org.usfirst.frc.team4611.robot.subsystems.Climber;
-import org.usfirst.frc.team4611.robot.subsystems.DriveTrain;
-import org.usfirst.frc.team4611.robot.subsystems.Elevator;
-import org.usfirst.frc.team4611.robot.subsystems.FancyLights;
-import org.usfirst.frc.team4611.robot.subsystems.Optical;
-import org.usfirst.frc.team4611.robot.subsystems.Solenoid;
-import org.usfirst.frc.team4611.robot.subsystems.UltrasonicSensor;
+import org.usfirst.frc.team4611.robot.subsytems.iMecanum;
+import org.usfirst.frc.team4611.robot.subsytems.tMecanum;
+import org.usfirst.frc.team4611.robot.subsytems.vMecanum;
 
-import edu.wpi.cscore.UsbCamera;
-import edu.wpi.first.networktables.NetworkTable;
-import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.wpilibj.CameraServer;
-import edu.wpi.first.wpilibj.DriverStation;
-import edu.wpi.first.wpilibj.I2C.Port;
 import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
+import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the manifest file in the resource
- * directory.
- */
 public class Robot extends IterativeRobot {
 
-	public static DriveTrain mecanum;
-	public static Elevator elevator;
-	public static Arm arm;
-	public static UltrasonicSensor ultrasonic;
-	public static Spark lightController;
-	public static FancyLights fancyLight;
-	public static Solenoid sol;
-	public static NetworkTableInstance tableInstance;
-	public static NetworkTable table;
-	public static Optical opt;
-	public static UsbCamera camera;
-	public static BoxPusher boxPusher;
-	public static DriverStation driver;
-	public static Climber climber;
-	public static OI oi;
-	public boolean hasInitialized = false;
-	public String autonFinalDecision;
-	private String fms = null;
-	public HashMap<String, Command> autonCommandGroup;
-	//public SendableChooser chooser;
-
+	public String motorControllerType;
 	Command autonomousCommand;
-	public static Command lightsCommand;
 	SendableChooser<Command> chooser = new SendableChooser<>();
+	public static iMecanum mecanum;
+	public static OI oi;
 	
-	
-	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
-	 */
 	@Override
 	public void robotInit() {
-		RobotMap.init(); //Run the method "init" in RobotMap
-		//Initialize utilities
-		mecanum = new DriveTrain();
-		elevator = new Elevator();
-		arm = new Arm();
-		sol = new Solenoid();
-		boxPusher = new BoxPusher();
-		ultrasonic = new UltrasonicSensor();
-		opt = new Optical(Port.kMXP);
-		lightController = new Spark(9);
-		fancyLight = new FancyLights();
-		climber = new Climber();
-		driver = DriverStation.getInstance();
-		autonCommandGroup = new AutonCommandGroup<String, Command>();
-			
+		//Initialize the subsystems
+		if(motorControllerType.toLowerCase().equals("t")) {
+			mecanum = new tMecanum();
+		}
+		else {
+			mecanum = new vMecanum();
+		}
 		oi = new OI();
-		
-		camera = CameraServer.getInstance().startAutomaticCapture();	
-		camera.setResolution(320, 240);
-		camera.setFPS(20);
-		camera.setExposureManual(35);
-		
-		// Set up default values for auton
-		RobotMap.updateValue(RobotMap.autonSubTable, RobotMap.strategy, "");
-		RobotMap.updateValue(RobotMap.autonSubTable, RobotMap.velocityRecordingTag, "");
-		
 	}
 
-	/**
-	 * This function is called once each time the robot enters Disabled mode.
-	 * You can use it to reset any subsystem information you want to clear when
-	 * the robot is disabled.
-	 */
 	@Override
 	public void disabledInit() {
-		RobotMap.defaults.saveProperties();
-		Logger.log("ROBOT DISABLED", "Robot");
-		
-		Logger.commit();
+
 	}
 
 	@Override
 	public void disabledPeriodic() {
-		Scheduler.getInstance().run();		
+		Scheduler.getInstance().run();
 	}
 
 	@Override
 	public void autonomousInit() {
-		String path = getPath();
-		
-		Logger.log("FMS INPUT [" + fms + "]");
-		Logger.log("Auton Final Decision [ "+path + "]", this.getClass().getName());
-		
-		autonomousCommand = autonCommandGroup.get(path);
-		
-		//autonomousCommand = new TestBlock();
-		
-		if (autonomousCommand == null) {
-			autonomousCommand = this.autonCommandGroup.get("DRIVEFORWARD");
-		}
+		autonomousCommand = chooser.getSelected();
 
-		if (autonomousCommand != null) {
+		if (autonomousCommand != null)
 			autonomousCommand.start();
-		}
-		
 	}
 
-	/**
-	 * This function is called periodically during autonomous
-	 */
 	@Override
 	public void autonomousPeriodic() {
 		Scheduler.getInstance().run();
@@ -149,112 +62,18 @@ public class Robot extends IterativeRobot {
 		// teleop starts running. If you want the autonomous to
 		// continue until interrupted by another command, remove
 		// this line or comment it out.
-		Robot.mecanum.setRampRate(0);
-		if (autonomousCommand != null) 
+		if (autonomousCommand != null)
 			autonomousCommand.cancel();
 	}
 
-	/**
-	 * This function is called periodically during operator control
-	 */
 	@Override
 	public void teleopPeriodic() {
-		Scheduler.getInstance().run();	
-		//Logger.log("Elevator Position: " + RobotMap.elevator_Talon.getSelectedSensorPosition(0), "Elevator");
+		Scheduler.getInstance().run();
 	}
+
 
 	@Override
 	public void testPeriodic() {
-	}
-	
-	public String getPath() {
-		fms= driver.getGameSpecificMessage();
-		for(int i = 0; (i < 10) && (fms == null || fms.isEmpty()); i++) {
-			try {
-				Thread.sleep(25);
-			} catch (InterruptedException e) {
-				Logger.log(e.getMessage(), "Sleep");
-			}
-			fms = driver.getGameSpecificMessage().trim();
-		}
-		if(fms == null || fms.isEmpty()) {
-			fms = "XXX";
-		}
-		String sw = fms.substring(0, 1);
-		String sc = fms.substring(1, 2);
-		String key = "";
-		boolean split = false;
-		
-		try {
-			String strat = ((String) RobotMap.getValue(RobotMap.autonSubTable, RobotMap.strategy)).trim().toUpperCase();
-			Logger.log(strat, "Auton Strat");
-			String position = strat.substring(0, 1).toUpperCase();  //L, R, C
-			String mode = strat.substring(1, 2).toUpperCase(); //P, T
-			String target1 = strat.substring(2, 4).toUpperCase(); //SW , SC
-			String target1Side = getSide(target1); //L, R
-			String target2 = strat.substring(4, 6).toUpperCase();//SW , SC
-			String target2Side = getSide(target2); //L, R
-			String oppTarget1 = strat.substring(6).toUpperCase();//SW , SC
-			String oppTarget1Side = getSide(oppTarget1); //L, R
-									
-			if(!(target1Side.equals(target2Side))) { //Are targets (not (same side))
-				split = true;
-			}
-			
-			if (mode.equals("T")) { //Target construction follows L + S(T1) + T1 + S(T2) + T2
-				key = position + target1Side + target1 + target2Side + target2;
-			}
-			
-			if (mode.equals("P")) {
-				if (split == true) { //If we're split
-					if (sw.equals(position)) { //and switch is close
-					key = position + sw + "SW" + position + "SC"; //go switch close then pick up box
-					} //RRSWRSC
-					else if (sc.equals(position)) { //or scale is close
-						key = position + sc + "SC" + position + "SC"; //go scale then pick up box
-					}
-				}
-				
-				else { // not split
-					if (sw.equals(position) && sc.equals(position)) { //on our side
-						key = position + target1Side + target1 + target2Side + target2; //go for targets
-					}
-					
-					else { //opp side
-						if(oppTarget1.equals("XX") || oppTarget1.isEmpty()) {
-							key = "DRIVEFORWARD";
-						}
-						else {
-							key = position + oppTarget1Side + oppTarget1 + oppTarget1Side + "SC"; //go for opp target then scale
-						}
-						
-					}
-				}
-			}
-			
-			return key;
-			
-		}catch(StringIndexOutOfBoundsException e) {
-			Logger.log("String out of bounds at getPath substrings caught", "Auton selection");
-		}
-		
-		
-		if(!(key.equals(null))) {
-			return key.trim().toUpperCase();
-		}
-		
-		return key;
-	}
-	
-	public String getSide(String target) {
-		String fms = driver.getGameSpecificMessage();
-		if (target.equals("SW")) {
-			return fms.substring(0, 1);
-		}
-		if (target.equals("SC")) {
-			return fms.substring(1, 2);
-		}
-		
-		return null;
+		LiveWindow.run();
 	}
 }
