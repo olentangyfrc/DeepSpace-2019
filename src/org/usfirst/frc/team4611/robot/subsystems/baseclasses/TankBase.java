03e67d213243f90a30cb107b13bfe8056d9e02d0
Connor2573 <connor.park2573@gmail.com>
2018-11-25 16:46:18 -0500

    second commit

---
+++ src/org/usfirst/frc/team4611/robot/subsystems/baseclasses/TankBase.java
@@ -0,0 +1,21 @@
+package org.usfirst.frc.team4611.robot.subsystems.baseclasses;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+import jaci.pathfinder.Trajectory;
+
+public abstract class TankBase extends Subsystem{
+	public abstract void move();
+	public abstract void moveForward(double speed);
+	public abstract void moveBackward(double speed);
+	public abstract void resetEncoders();
+	
+	public abstract void rotate(double velocity);
+	public abstract int getAverageSensorPos();
+	
+	public abstract void moveVelocityAuton(double YVal);
+	public abstract double getVelocity();
+	
+	/*public abstract void followTrajectory(Trajectory t);
+	
+	public abstract void setTrajectorySpeeds();*/
+}
