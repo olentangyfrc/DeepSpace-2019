d4d8a0606c797fcb6425dbc466d1de4fd5e693b3
NotSketchyBen <brickman2357@gmail.com>
2019-01-07 09:29:14 -0500

    Setting up Navigation

--- src/main/java/org/usfirst/frc/team4611/robot/subsystems/navigation/commands/TurnLeft.java
+++ src/main/java/org/usfirst/frc/team4611/robot/subsystems/navigation/commands/TurnLeft.java
@@ -1,6 +1,8 @@
-package org.usfirst.frc.team4611.robot.commands.auton.pigeon;
+package org.usfirst.frc.team4611.robot.subsystems.navigation.commands;
 
 import org.usfirst.frc.team4611.robot.Robot;
+import org.usfirst.frc.team4611.robot.subsystems.SubsystemFactory;
+import org.usfirst.frc.team4611.robot.subsystems.navigation.sensors.Pigeon;
 
 import edu.wpi.first.wpilibj.command.Command;
 
@@ -14,9 +16,13 @@ public class TurnLeft extends Command {
 	private final double rotationDifference = 0;
 	private final double ANGLE_TOLERANCE = 1;
 
+	private Pigeon rotationPigeon;
+
 	public TurnLeft(double angle) {
 		this.angle = angle;
-		this.requires(Robot.driveTrain);
+		this.requires(SubsystemFactory.getInstance().getDriveTrain());
+
+
 	}
 
 	/**
@@ -25,7 +31,7 @@ public class TurnLeft extends Command {
 	 */
 	protected void initialize() {
 		// Gets the current angle once the command begins
-		startingPigeonAngle = Robot.rotationPigeon.getCurrentAngle();
+		startingPigeonAngle = rotationPigeon.getCurrentAngle();
 
 		// Desired angle is the difference between where we start and the angle to the
 		// box
@@ -41,9 +47,9 @@ public class TurnLeft extends Command {
 	 */
 	protected void execute() {
 		System.out.println(desiredAngle);
-		System.out.println(Robot.rotationPigeon.getCurrentAngle());
+		System.out.println(rotationPigeon.getCurrentAngle());
 		// How far do we have to go b4 we get to the target?
-		double errorAngle = Robot.rotationPigeon.getAbolsuteAngleError(desiredAngle);
+		double errorAngle = rotationPigeon.getAbolsuteAngleError(desiredAngle);
 
 		// How do we respond to that error?
 		double pVal = errorAngle * .15;
@@ -54,7 +60,7 @@ public class TurnLeft extends Command {
 		// Check to see if we are where we need to be before we even move. we might be
 		// there.
 		if (!isFinished()) {
-			Robot.driveTrain.rotate(-speed);
+			SubsystemFactory.getInstance().getDriveTrain().rotate(-speed);
 		}
 	}
 
@@ -65,12 +71,12 @@ public class TurnLeft extends Command {
 
 		// Checks to see if the bot has turned the desired amount within a certain
 		// tolerance
-		if (Robot.rotationPigeon.getAbolsuteAngleError(desiredAngle) <= ANGLE_TOLERANCE) {
+		if (rotationPigeon.getAbolsuteAngleError(desiredAngle) <= ANGLE_TOLERANCE) {
 			return true;
 		}
 
 		// Checks to see if the bot has turned farther than before
-		if (Robot.rotationPigeon.getCurrentAngle() > desiredAngle) {
+		if (rotationPigeon.getCurrentAngle() > desiredAngle) {
 			return true;
 		}
 		return false;
