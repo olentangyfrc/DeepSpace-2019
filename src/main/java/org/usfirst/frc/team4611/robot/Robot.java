9c22283e3eaebfb05b763f8423c214f5beb0d694
Patrick Worth <worthp@worthconsulting.com>
2019-01-17 02:01:12 -0500

    connected to vision network table

--- src/main/java/org/usfirst/frc/team4611/robot/Robot.java
+++ src/main/java/org/usfirst/frc/team4611/robot/Robot.java
@@ -58,13 +58,6 @@ public class Robot extends TimedRobot {
 
 	@Override
 	public void teleopInit() {
-		// This makes sure that the autonomous stops running when
-		// teleop starts running. If you want the autonomous to
-		// continue until interrupted by another command, remove
-		// this line or comment it out.
-		isOn = true;
-		// if (autonomousCommand != null)
-		// autonomousCommand.cancel();
 	}
 	@Override
 	public void teleopPeriodic() {
--- src/main/java/org/usfirst/frc/team4611/robot/subsystems/vision/commands/PollNetworkTable.java
+++ src/main/java/org/usfirst/frc/team4611/robot/subsystems/vision/commands/PollNetworkTable.java
@@ -3,8 +3,6 @@ package org.usfirst.frc.team4611.robot.subsystems.vision.commands;
 import org.usfirst.frc.team4611.robot.subsystems.SubsystemFactory;
 import org.usfirst.frc.team4611.robot.subsystems.vision.Vision;
 
-import javax.sound.sampled.BooleanControl;
-
 import org.usfirst.frc.team4611.robot.networktables.NetTableManager;
 
 
@@ -14,20 +12,24 @@ public class PollNetworkTable extends Command{
 
     private Vision  vision;
     public PollNetworkTable() {
+        vision = SubsystemFactory.getInstance().getVision();
+        requires(vision);
 
     }
 
     public void init() {
-        vision = SubsystemFactory.getInstance().getVision();
     }
 
     public void execute() {
-        System.out.println("Polling Vision network table");
+       /*
         vision.setTapeFound((Boolean)NetTableManager
                 .getValue("Vision", "tapeFound", new Boolean(false)))
                 ;
         vision.setBallFound((Boolean)NetTableManager
                 .getValue("Vision", "tapeFound", new Boolean(false)));
+        */
+
+        System.out.println(NetTableManager.getValue("Vision", "connected", "no"));
     }
 
     public void end() {
