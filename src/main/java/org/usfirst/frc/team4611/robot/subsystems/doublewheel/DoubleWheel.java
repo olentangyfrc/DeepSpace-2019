0ba5e7534f3ba0419c78798a37a727a7896a6462
NotSketchyBen <brickman2357@gmail.com>
2019-01-21 11:02:06 -0500

    Added Double wheel subsystem

---
+++ src/main/java/org/usfirst/frc/team4611/robot/subsystems/doublewheel/DoubleWheel.java
@@ -0,0 +1,37 @@
+package org.usfirst.frc.team4611.robot.subsystems.doublewheel;
+
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
+import org.usfirst.frc.team4611.robot.subsystems.PortMan;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class DoubleWheel extends Subsystem {
+
+    private WPI_TalonSRX wheelIntakeLeft;
+    private WPI_TalonSRX wheelIntakeRight;
+
+    public void init(PortMan pm) throws Exception {
+        wheelIntakeLeft = new WPI_TalonSRX(pm.acquirePort(PortMan.can_18_label, "DoubleWheel.leftWheelIntake"));
+        wheelIntakeRight = new WPI_TalonSRX(pm.acquirePort(PortMan.can_19_label, "DoubleWheel.rightWheelIntake"));
+
+        wheelIntakeLeft.config_kP(0, .5, 0);
+        wheelIntakeLeft.config_kI(0, 0, 0);
+        wheelIntakeLeft.config_kD(0, 0, 0);
+        wheelIntakeLeft.config_kF(0, 0, 0);
+        wheelIntakeLeft.configMotionCruiseVelocity(4096, 0);
+        wheelIntakeLeft.configMotionAcceleration(4096,0);
+    
+        wheelIntakeRight.follow(wheelIntakeLeft);
+        wheelIntakeRight.setInverted(true);
+    }
+
+
+    @Override
+    protected void initDefaultCommand() {
+
+    }
+
+
+
+}
