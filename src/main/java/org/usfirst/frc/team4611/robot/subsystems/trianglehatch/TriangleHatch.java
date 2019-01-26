8503442390c3db3b192e95035cb95cf6347e408c
Rahul <rahul@Rahair.local>
2019-01-26 11:23:33 -0500

    ShuffleBoardTab has been added to TriangleHatch

--- src/main/java/org/usfirst/frc/team4611/robot/subsystems/trianglehatch/TriangleHatch.java
+++ src/main/java/org/usfirst/frc/team4611/robot/subsystems/trianglehatch/TriangleHatch.java
@@ -2,7 +2,6 @@ package org.usfirst.frc.team4611.robot.subsystems.trianglehatch;
 
 import java.util.logging.Logger;
 
-import org.usfirst.frc.team4611.robot.networktables.NetTableManager;
 import org.usfirst.frc.team4611.robot.subsystems.PortMan;
 import org.usfirst.frc.team4611.robot.subsystems.trianglehatch.commands.RetractHatch;
 
@@ -17,33 +16,32 @@ public class TriangleHatch extends Subsystem {
 
     private DoubleSolenoid pusher;
 
+    private ShuffleboardTab tab;
+    
+    private NetworkTableEntry triangleStatus;
+
     public TriangleHatch() {  
     }
-
-    private ShuffleboardTab tab;
-    private NetworkTableEntry triangleStatus;
-    
-
 
     public void init(PortMan pm) throws Exception {
            logger.info("initializing");
            pusher = new DoubleSolenoid(pm.acquirePort(PortMan.pcm0_label, "TriangleHatch.inDoubleSolenoid"), pm.acquirePort(PortMan.pcm1_label, "TriangleHatch.outDoubleSolenoid"));
 
-           tab = Shuffleboard.getTab("Health Map");
-        NetTableManager.updateValue("Health Map", "Triangle Initialize", true);
-        
-        triangleStatus = tab.add("Triangle Engaged", false).getEntry();
-    } 
+        triangleStatus = tab.add("Triangle Enganged", false).getEntry();
+    }
 
     public void pushHatch() {
         pusher.set(DoubleSolenoid.Value.kForward);
+        triangleStatus.setBoolean(true);
     }
 
     public void retractPistons() {
         pusher.set(DoubleSolenoid.Value.kReverse);
-    }
+        triangleStatus.setBoolean(true);
+        }
 
     public boolean isRetracted() {
+        triangleStatus.setBoolean(false);
         return pusher.get().equals(DoubleSolenoid.Value.kReverse);
     }
 
