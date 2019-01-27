b532f0a3b82aaf06b8c5dac78ad886c1a8cd2c06
Connor2573 <connor.park2573@gmail.com>
2019-01-27 14:56:43 -0500

    adding untracked files

---
+++ src/main/java/org/usfirst/frc/team4611/robot/subsystems/WheelIntake/commands/StopWheelIntake.java
@@ -0,0 +1,29 @@
+package org.usfirst.frc.team4611.robot.subsystems.WheelIntake.commands;
+
+import org.usfirst.frc.team4611.robot.subsystems.SubsystemFactory;
+import org.usfirst.frc.team4611.robot.subsystems.WheelIntake.WheelIntake;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class StopWheelIntake extends Command {
+
+    private WheelIntake wheelIntake;
+
+    public StopWheelIntake() {
+        wheelIntake = SubsystemFactory.getInstance().getWheelIntake();
+    }
+
+    protected void initialize() {
+    }
+
+    @Override
+    protected void execute() {
+        wheelIntake.moveIntake(0);
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+    
+}
