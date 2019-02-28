8e6c92354f1ca82e7511e50988528d7d20ecc00a
worthp <worthp@worthconsulting.com>
2019-02-27 23:13:58 -0500

    quick fix

--- src/main/java/org/usfirst/frc/team4611/robot/subsystems/driverfeedback/commands/DriverFeedbackDefault.java
+++ src/main/java/org/usfirst/frc/team4611/robot/subsystems/driverfeedback/commands/DriverFeedbackDefault.java
@@ -34,7 +34,7 @@ public class DriverFeedbackDefault extends Command {
   // Make this return true when this Command no longer needs to run execute()
   @Override
   protected boolean isFinished() {
-    return false;
+    return true;
   }
 
   // Called once after isFinished returns true
