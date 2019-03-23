517813cad97a358da6a9d519974fc95a444b2bb1
Alpha <connor.park@hotmail.com>
2019-03-23 14:48:34 -0400

    removing football mac address force

--- src/main/java/org/usfirst/frc/team4611/robot/subsystems/SubsystemFactory.java
+++ src/main/java/org/usfirst/frc/team4611/robot/subsystems/SubsystemFactory.java
@@ -121,7 +121,6 @@ public class SubsystemFactory {
         logger.info("intializing");
         
         botMacAddress   = System.getenv("MAC_ADDRESS");
-        botMacAddress = footballMacAddress;
         logger.info("["+botMacAddress+"]");
         if (botMacAddress == null) {
             throw new OzoneException("Could not find MAC Address for this bot. Make sure /home/lvuser/.bash_profile is correct");
